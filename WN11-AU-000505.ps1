d3f57be51f768654e0ca4845112cdf5056bbcd49
tomarmickey <tomarmickey@gmail.com>
2026-04-16 23:44:21 -0400

    Update WN11-AU-000505.ps1

--- WN11-AU-000505.ps1
+++ WN11-AU-000505.ps1
@@ -1,14 +1,9 @@
-<#
-.SYNOPSIS
-    Remediation for WN11-AU-000505 - Security Log Max Size.
-.DESCRIPTION
-    Sets the maximum size of the Security event log to 196608 KB per DISA STIG requirements.
-.AUTHOR
-    Tomar Mickey
-#>
-
 $path = "HKLM:\Software\Policies\Microsoft\Windows\EventLog\Security"
 if (-not (Test-Path $path)) {
     New-Item -Path $path -Force
 }
-New-ItemProperty -Path $path -Name "MaxSize" -Value 196608 -PropertyType DWord -Force
+# Setting to 1024000 to match your Tenable audit requirement
+New-ItemProperty -Path $path -Name "MaxSize" -Value 1024000 -PropertyType DWord -Force
+
+# Restart the service to ensure the system recognizes the change
+Restart-Service -Name EventLog -Force
